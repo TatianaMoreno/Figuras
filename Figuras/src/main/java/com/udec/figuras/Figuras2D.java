254e6ca8a2c11a6d77eb36903427e6f89bc88eb8
AndresChila <AndresChila@DESKTOP-BK0R245>
2019-08-31 09:23:42 -0500

    No se que pasa al clonar no descarga nada, vuelvo a añadir las clases

---
+++ Figuras/src/main/java/com/udec/figuras/Figuras2D.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udec.geometricas;
+
+/**
+ *
+ * @author Tatiana Moreno, Andres Chila
+ */
+public abstract class Figuras2D extends Figuras{
+    private double perimetro;
+    
+}
---
+++ Figuras/src/main/java/com/udec/figuras/Figuras3D.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udec.geometricas;
+
+/**
+ *
+ * @author Tatiana Moreno, Andres Chila
+ */
+public abstract class Figuras3D extends Figuras{
+    
+}
